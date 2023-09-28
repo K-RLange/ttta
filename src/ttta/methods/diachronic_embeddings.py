b48af9bbeaa9d8eab51f6228e8489f014d82ebeb
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-09-28 17:29:36 +0200

    fixed word2vec.py

--- src/ttta/methods/diachronic_embeddings.py
+++ src/ttta/methods/diachronic_embeddings.py
@@ -1,12 +1,12 @@
 from utils.utils import *
+from glob import glob
 
 
 
 
 class DiachronicEmbeddings():
-    def __init__(self, **kwargs) -> None:
-        pass
-
+    def __init__(self, model) -> None:
+        self.model = model
 
 
     @classmethod
@@ -20,39 +20,63 @@ class DiachronicEmbeddings():
         Returns:
             DiachronicEmbeddings: A DiachronicEmbeddings object.
         """
-        return cls(**kwargs)
 
+        from word2vec.word2vec import Word2VecModel
+        return cls(model = Word2VecModel(**kwargs))
 
-
-
-    def get_model(self, model_name):
-        if model_name == 'Word2Vec':
-            from word2vec.word2vec import Word2VecModel
-            return Word2VecModel()
         
-        else:
-            raise ValueError('model unknown')
-        
-
 
     
-    def train(self, corpus):
+    def train(self, corpora: list, align: bool = True, ref: int = -1, **kwargs) -> None:
         """
         Train the model.
 
         Args:
+            corpora: A list of lists (each one is a corpus).
             **kwargs: Arbitrary keyword arguments.
-        """
-        pass
 
-    def load(self, path):
+        """
+        raw = []
+        for corpus in corpora:
+            m = self.model
+            m.train(corpus, **kwargs)
+            raw.append(m)
+            del m
+
+        print('len raw', len(raw))
+        
+        if align:
+            aligned = []
+            reference_model = raw[ref]
+            for i in range(len(raw)):
+                print(i)
+                if i == ref:
+                    aligned.append(reference_model)
+                    print('skipped')
+                    continue
+                new = raw[i].align(reference_model = reference_model, method = 'procrustes')
+                aligned.append(new)
+                del new
+
+        
+        return {'raw': raw, 'aligned': aligned, 'reference_model': reference_model}
+    
+
+        
+
+    def load(self, model_dir: str, **kwargs):
         """
         Load the model.
 
         Args:
             **kwargs: Arbitrary keyword arguments.
         """
-        pass
+
+        paths = sorted(glob(model_dir + '/*.model'))
+        models = [self.model.load(path) for path in paths]
+        
+
+
 
     def predict(self, **kwargs):
         """
@@ -63,6 +87,23 @@ class DiachronicEmbeddings():
         """
         pass
 
+
+
+    # def load(self, path):
+    #     return Word2Vec.load(path)
+    
+    def save(self, model, path):
+        model.save(path)
+
+    
+    # def get_embedding(self, model, word):
+    #     return model.wv[word]
+    
+    # def most_similar(self, model, word):
+    #     return model.wv.most_similar(word)
+    
+    # def similarity(self, model, word1, word2):
+    #     return model.wv.similarity(word1, word2)
             
 
     
@@ -71,8 +112,39 @@ class DiachronicEmbeddings():
     
         
 if __name__ == '__main__':
-    w2v = DiachronicEmbeddings.from_word2vec()
- 
-    w2v.train()
-    w2v.load()
-    w2v.predict()
+    corpus_1980 = read_txt('../../../input/1980_articles.txt')[:10]
+    corpus_1990 = read_txt('../../../input/1990_articles.txt')[:10]
+    corpus_2000 = read_txt('../../../input/2000_articles.txt')[:10]
+    corpus_2010 = read_txt('../../../input/2010_articles.txt')[:10]
+
+    corpora = [corpus_1980, corpus_1990, corpus_2000, corpus_2010]
+    print('loaded corpora')
+
+
+    w2v = DiachronicEmbeddings.from_word2vec(window = 10)
+
+
+    print('initialized model')
+    print('training models')
+    
+    models = w2v.train(corpora=corpora, align = True, epochs = 2)
+    print('trained models')
+
+    
+
+
+    w2v.save(models['reference_model'], 'reference_model.model')
+    print('saved reference model')
+
+    model = w2v.load('reference_model.model')
+
+
+
+    
+    
+    
+
+
+
+
+    
--- src/ttta/methods/word2vec/word2vec.py
+++ src/ttta/methods/word2vec/word2vec.py
@@ -1,142 +1,180 @@
 from gensim.models import Word2Vec
-from src.ttta.methods.utils.utils import read_yaml, read_txt, smart_procrustes_align_gensim
-import os
-
-
-
+from utils.utils import *
 
 class Word2VecModel():
 
-    def __init__(self, **kwargs) -> None:
-        pass
-        
-    
-    def build_vocab(self, model, sentences):
-        model.build_vocab(sentences)
-        
-    
-    def train(self, model, sentences, **kwargs):
-
-        total_examples = model.corpus_count
-        
-        model.train(
-            sentences, 
-            total_examples=total_examples, 
-            **kwargs)
-        
-    def load(self, path):
-        return Word2Vec.load(path)
-    
-    def save(self, model, path):
-        model.save(path)
+    def __init__(self, 
+                min_count = 5,
+                window = 5,
+                negative = 5,
+                ns_exponent = 0.75,
+                vector_size = 300,
+                sg = 1,
+                workers = 1,
+                **kwargs):
+        self.word2vec_model = Word2Vec(
+            min_count = min_count,
+            window = window,
+            negative = negative,
+            ns_exponent = ns_exponent,
+            vector_size = vector_size,
+            sg = sg,
+            workers = workers,
+            **kwargs
+        )
 
     
-    # def embedding(self, model, word):
-    #     return model.wv[word]
+    def train(self, 
+              corpus: list, 
+              epochs = 10, 
+              start_alpha = 0.025, 
+              end_alpha = 0.0001,
+              compute_loss = True,
+              **kwargs):
     
-    # def most_similar(self, model, word):
-    #     return model.wv.most_similar(word)
+
+        self.word2vec_model.build_vocab(corpus)
+        total_examples = self.word2vec_model.corpus_count
+
+        self.word2vec_model.train(
+            corpus,
+            total_examples=total_examples,
+            epochs= epochs,
+            start_alpha= start_alpha,
+            end_alpha= end_alpha,
+            compute_loss= compute_loss,
+            **kwargs
+        )
+        
+        return self.word2vec_model
     
-    # def similarity(self, model, word1, word2):
-    #     return model.wv.similarity(word1, word2)
+
+    def align(self, reference_model, **kwargs):
+        """
+        Align the model.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments.
+        """
+
+        try:
+            if kwargs['method'] == 'procrustes':
+                return reference_model, smart_procrustes_align_gensim(reference_model.word2vec_model,self.word2vec_model)
+            
+            else:
+                raise NotImplementedError('Only procrustes method is implemented')
+        
+        except KeyError:
+            raise ValueError('method must be specified')
+        
+    
+    def save(self, path):
+        """
+        Save the model.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments.
+        """
+        self.word2vec_model.save(path)
+            
 
     
-    def pipeline(self, **kwargs):
-        config_path = kwargs.get('config', 'word2vec.yaml')
-        config = read_yaml(config_path)['Word2Vec']
+    # def pipeline(self, **kwargs):
+    #     config_path = kwargs.get('config', 'word2vec.yaml')
+    #     config = read_yaml(config_path)['Word2Vec']
 
         
-        training_config = config['pipeline']['training']
-        alignment_config = config['pipeline']['alignment']
+    #     training_config = config['pipeline']['training']
+    #     alignment_config = config['pipeline']['alignment']
         
-        
-
-
-        periods = kwargs.get('periods', None)
-        if periods is None:
-            raise ValueError('periods must be specified')
-        elif not isinstance(periods, list):
-            raise ValueError('periods must be a list of strings')
-        
-        train = kwargs.get('train', False)
-        align = kwargs.get('align', False)
-        
-
-        if train:
-            training_output_dir = kwargs.get('training_output_dir', None)
-            # f'{directory}/output/word2vec/{trial_name}/train'
-
-            if training_output_dir is None:
-                raise ValueError('training_output_dir must be specified')
-            elif not isinstance(training_output_dir, str):
-                raise ValueError('training_output_dir must be string')
-            
-            if not os.path.exists(training_output_dir):
-                os.makedirs(training_output_dir)
-            
-            print('Training Word2Vec models. Saving to: ', training_output_dir)
-
-            corpus = kwargs.get('corpus', None)
-            if corpus is None:
-                raise ValueError('Training requires corpus as input')
-            
-            elif not isinstance(corpus, list):
-                raise ValueError('corpus must list of lists of strings')
-            
-            if len(corpus) != len(periods):
-                raise ValueError('corpus must be same length as periods')
-            
-            
-
-            for period_number, period in enumerate(periods):
-                word2vec_model = Word2Vec(**training_config['model_params'])
-                self.build_vocab(word2vec_model, corpus[period_number])
-                self.train(word2vec_model, corpus[period_number], **training_config['training_params'])
-                self.save(word2vec_model, path= training_output_dir + f'/raw_{period}.model')
-
-            print('Training complete.')
         
 
 
-        if align:
-            alignment_output_dir = kwargs.get('alignment_output_dir', None)
-            # f'{directory}/output/word2vec/{trial_name}/align'
+    #     periods = kwargs.get('periods', None)
+    #     if periods is None:
+    #         raise ValueError('periods must be specified')
+    #     elif not isinstance(periods, list):
+    #         raise ValueError('periods must be a list of strings')
+        
+    #     train = kwargs.get('train', False)
+    #     align = kwargs.get('align', False)
+        
 
-            if alignment_output_dir is None:
-                raise ValueError('alignment_output_dir must be specified')
-            elif not isinstance(alignment_output_dir, str):
-                raise ValueError('alignment_output_dir must be string')
+    #     if train:
+    #         training_output_dir = kwargs.get('training_output_dir', None)
+    #         # f'{directory}/output/word2vec/{trial_name}/train'
+
+    #         if training_output_dir is None:
+    #             raise ValueError('training_output_dir must be specified')
+    #         elif not isinstance(training_output_dir, str):
+    #             raise ValueError('training_output_dir must be string')
             
-            if not os.path.exists(alignment_output_dir):
-                os.makedirs(alignment_output_dir)
+    #         if not os.path.exists(training_output_dir):
+    #             os.makedirs(training_output_dir)
+            
+    #         print('Training Word2Vec models. Saving to: ', training_output_dir)
+
+    #         corpus = kwargs.get('corpus', None)
+    #         if corpus is None:
+    #             raise ValueError('Training requires corpus as input')
+            
+    #         elif not isinstance(corpus, list):
+    #             raise ValueError('corpus must list of lists of strings')
+            
+    #         if len(corpus) != len(periods):
+    #             raise ValueError('corpus must be same length as periods')
             
             
 
-            all_models_path = kwargs.get('alignment_models_path', None)
-            if all_models_path is None:
-                raise ValueError('alignment_models_path must be specified')
-            elif not isinstance(all_models_path, str):
-                raise ValueError('alignment_models_path must be string')
+    #         for period_number, period in enumerate(periods):
+    #             word2vec_model = Word2Vec(**training_config['model_params'])
+    #             self.build_vocab(word2vec_model, corpus[period_number])
+    #             self.train(word2vec_model, corpus[period_number], **training_config['training_params'])
+    #             self.save(word2vec_model, path= training_output_dir + f'/raw_{period}.model')
+
+    #         print('Training complete.')
+        
+
+
+    #     if align:
+    #         alignment_output_dir = kwargs.get('alignment_output_dir', None)
+    #         # f'{directory}/output/word2vec/{trial_name}/align'
+
+    #         if alignment_output_dir is None:
+    #             raise ValueError('alignment_output_dir must be specified')
+    #         elif not isinstance(alignment_output_dir, str):
+    #             raise ValueError('alignment_output_dir must be string')
+            
+    #         if not os.path.exists(alignment_output_dir):
+    #             os.makedirs(alignment_output_dir)
+            
+            
+
+    #         all_models_path = kwargs.get('alignment_models_path', None)
+    #         if all_models_path is None:
+    #             raise ValueError('alignment_models_path must be specified')
+    #         elif not isinstance(all_models_path, str):
+    #             raise ValueError('alignment_models_path must be string')
             
 
         
-            all_models = [self.load(model) for model in sorted(glob(all_models_path + '/*.model'))]
+    #         all_models = [self.load(model) for model in sorted(glob(all_models_path + '/*.model'))]
 
-            reference_model = all_models[alignment_config['reference']]
-            reference_period = periods[alignment_config['reference']]
-            self.save(reference_model, path= alignment_output_dir + f'/aligned_{reference_period}.model')
+    #         reference_model = all_models[alignment_config['reference']]
+    #         reference_period = periods[alignment_config['reference']]
+    #         self.save(reference_model, path= alignment_output_dir + f'/aligned_{reference_period}.model')
 
-            alignment_models = all_models[:alignment_config['reference']] + all_models[alignment_config['reference'] + 1:]
-            alignment_periods = periods[:alignment_config['reference']] + periods[alignment_config['reference'] + 1:]
+    #         alignment_models = all_models[:alignment_config['reference']] + all_models[alignment_config['reference'] + 1:]
+    #         alignment_periods = periods[:alignment_config['reference']] + periods[alignment_config['reference'] + 1:]
 
-            for model, period in zip(alignment_models, alignment_periods):
-                if alignment_config['method'] == 'orthogonal_procrustes':
-                    aligned_model = smart_procrustes_align_gensim(reference_model,model)
-                else:
-                    raise ValueError('alignment method not supported')
-                self.save(aligned_model, path= alignment_output_dir + f'/aligned_{period}.model')
+    #         for model, period in zip(alignment_models, alignment_periods):
+    #             if alignment_config['method'] == 'orthogonal_procrustes':
+    #                 aligned_model = smart_procrustes_align_gensim(reference_model,model)
+    #             else:
+    #                 raise ValueError('alignment method not supported')
+    #             self.save(aligned_model, path= alignment_output_dir + f'/aligned_{period}.model')
 
-        return training_output_dir if train else None, alignment_output_dir if align else None
+    #     return training_output_dir if train else None, alignment_output_dir if align else None
 
 
         
@@ -149,18 +187,29 @@ class Word2VecModel():
     
 
 if __name__ == '__main__':
-    corpus_2015 = read_txt('../../../../input/articles/1980_articles.txt')[:10]
-    w2v = Word2VecModel()
+    w2v = Word2VecModel(window = 10)
+
+    # corpus_2015 = read_txt('../../../../input/1980_articles.txt')[:10]
+    # w2v = Word2VecModel()
+    # w2v.train(corpus_2015, size= 100, window= 5, min_count= 1, workers= 4, sg= 0, iter= 5)
+    # w2v.save(w2v, 'output/word2vec/trial_1/train/raw_1980.model')
+    # model = w2v.load('output/word2vec/trial_1/train/raw_1980.model')
+    # print(model.wv['the'])
+    # print(model.wv.most_similar('the'))
+    # print(model.wv.similarity('the', 'then'))
+
+
+
     # _, _ = w2v.pipeline(periods = ['1980'], train= True, align = False, training_output_dir = '../../../../output/word2vec/trial_1/train', corpus = [corpus_2015])
 
-    _, _ = w2v.pipeline(periods = ['1980', '2015'], 
-                        train= True,
-                        training_output_dir = '../../../../output/word2vec/trial_1/train',
-                        corpus = [corpus_2015, corpus_2015],
-                        align = True,
-                        alignment_output_dir = '../../../../output/word2vec/trial_1/align',
-                        alignment_models_path = '../../../../output/word2vec/trial_1/train',
-                        )
+    # _, _ = w2v.pipeline(periods = ['1980', '2015'], 
+    #                     train= True,
+    #                     training_output_dir = '../../../../output/word2vec/trial_1/train',
+    #                     corpus = [corpus_2015, corpus_2015],
+    #                     align = True,
+    #                     alignment_output_dir = '../../../../output/word2vec/trial_1/align',
+    #                     alignment_models_path = '../../../../output/word2vec/trial_1/train',
+    #                     )
    
     
 
