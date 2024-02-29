35be3e8c007dc07d98fd3e476fa2637c97731814
K-RLange <101263876+K-RLange@users.noreply.github.com>
2024-02-29 11:33:06 +0100

    Bugfixes for LDA gibbs sampling

--- src/ttta/methods/LDA/vanilla_gibbs.h
+++ src/ttta/methods/LDA/vanilla_gibbs.h
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 void vanilla_gibbs(unsigned long long * w_vec, unsigned int * as_vec, unsigned long long * d_vec, unsigned long long * vk_mat,  unsigned long long * dk_mat,
-                   unsigned long long * v_sum, double * alpha, double * gamma, double * probs,
+                   unsigned long long * v_sum, double * alpha, double * gamma, long double * probs,
                    int K, long V, long D, long W, int iter, int start){
     unsigned int k0, k1;
     unsigned long long vv=0, dd=0, ww, ii;
-    double random;
+    long double random;
     srand(time(NULL));
 
     for (ww = 0; ww < W; ww++) {
@@ -27,17 +27,26 @@ void vanilla_gibbs(unsigned long long * w_vec, unsigned int * as_vec, unsigned l
             vv = w_vec[ww];
             dd=d_vec[ww];
             k0 = as_vec[ww];
+            k1 = k0;
             vk_mat[vv*K+k0]--;
             dk_mat[dd*K+k0]--;
             v_sum[k0]--;
-
-            probs[0] = (long double) (vk_mat[vv*K] + gamma[0]) * (dk_mat[dd*K] + alpha[0]) / (v_sum[0] + K * alpha[0] + V * gamma[0]) ;
+            probs[0] = (long double) (vk_mat[vv*K] + gamma[0]) * (dk_mat[dd*K] + alpha[0]) / (fmax(v_sum[0], 1) + V * gamma[0]);
 
             for (int kk = 1; kk < K; kk++) {
-                probs[kk] =(double) (vk_mat[vv*K+kk] + gamma[kk]) * (dk_mat[dd*K+kk] + alpha[kk]) / v_sum[kk] + probs[kk -1];
+                probs[kk] =(long double) (vk_mat[vv*K+kk] + gamma[kk]) * (dk_mat[dd*K+kk] + alpha[kk]) / (fmax(v_sum[kk], 1) + V * gamma[kk]) + probs[kk -1];
             }
+            if (probs[K-1]==0) {
+              printf("probs are equal to %f\n", probs[K-1]);
+              for (int newk = 0; newk < K; ++newk) probs[newk]=1.0/K;
+            }
+            // print the probs:
+//            for (int kk = 0; kk < K; kk++) {
+//                printf("%f ", probs[kk]);
+//            }
 
-            random = (double) rand() / (double) (RAND_MAX) * (double) probs[K-1];
+            random = (long double) rand() / (RAND_MAX) * probs[K-1];
+//            printf("%f\nprobs:", random);
             for (int kk = 0; kk < K; kk++) {
                 if (probs[kk] > random) {
                     k1 = kk;
@@ -68,18 +77,10 @@ void load_wk_mat(unsigned long long * w_vec, unsigned int * as_vec, unsigned lon
     unsigned int k0;
     unsigned long long vv=0, ww;
     srand(time(NULL));
-//    printf("%d ", W);
-//    printf("%d ", w_vec[0]);
-//    printf("%d ", w_vec[1]);
-//    printf("%d\n", w_vec[2]);
     for (ww = 0; ww < W; ww++) {
         vv = w_vec[ww];
         k0 = as_vec[ww];
         as_vec[ww] = k0;
-//        printf("%d ", k0);
-//        printf("%d ", vv);
-//        printf("%d ", vv * K);
-//        printf("%d\n", vv * K + k0);
         vk_mat[vv*K+k0]++;
     }
 }
