63edfab1ca3a4dab469c408af0ec873a4863322a
K-RLange <101263876+K-RLange@users.noreply.github.com>
2024-03-05 14:24:58 +0100

    Bugfixes for chunk creation arguments

--- src/ttta/preprocessing/chunk_creation.py
+++ src/ttta/preprocessing/chunk_creation.py
@@ -5,25 +5,41 @@ import warnings
 import numpy as np
 from ..methods.lda_prototype import LDAPrototype
 
-def _get_time_indices(texts: pd.DataFrame, how: Union[str, List[datetime]] = None, last_date: datetime = None,
+def _get_time_indices(texts: pd.DataFrame, how: Union[str, List[datetime]] = "M", last_date: datetime = None,
                       date_column: str = "date", min_docs_per_chunk: int = 500) -> pd.DataFrame:
     """
         Creates the time indices for the given texts. If update is True, the time indices are appended to the existing ones.
         Args:
             texts: A pandas DataFrame containing the documents and their respective dates.
                    The dates must be in a format interpretable by pandas.to_datetime(). The texts must be a list of strings.
-            update: Whether the time indices should be appended to the existing ones.
             how: List of datetime dates indicating the end of time chunks or a string indicating the frequency of the time chunks. Used to create time
                  chunks when fixed dates were used for the initial fit. If None, the same time chunk rule as in the initial fit is used.
+            last_date: The last date of the texts that were used before. If None, the last date of the texts is used.
+            date_column: The name of the column containing the dates in the texts DataFrame.
+            min_docs_per_chunk: The minimum number of documents that a time chunk must contain. If a time chunk does not contain at least this many
+                                documents, it is combined with the next time chunk.
     """
     if not isinstance(texts, pd.DataFrame):
         raise TypeError("texts must be a pandas DataFrame!")
-
+    if not isinstance(date_column, str):
+        raise TypeError("date_column must be a string!")
+    if not isinstance(min_docs_per_chunk, int):
+        try:
+            if min_docs_per_chunk == int(min_docs_per_chunk):
+                min_docs_per_chunk = int(min_docs_per_chunk)
+            else:
+                raise ValueError
+        except ValueError:
+            raise TypeError("min_docs_per_chunk must be an integer!")
+    if min_docs_per_chunk < 1:
+        raise ValueError("min_docs_per_chunk must be a natural number greater than 0!")
     df_index = pd.DataFrame({'chunk_start': range(len(texts))}, index=pd.to_datetime(texts[date_column]))
     if last_date is not None:
         if df_index.index.min() <= last_date:
             raise ValueError("The dates of the new texts chronologically overlap with the text that were already used before!")
 
+    if not isinstance(how, (str, list, np.ndarray)):
+        raise TypeError("how must be a string or a list of datetime objects!")
     if isinstance(how, str):
         period_start = df_index.resample(how).first()
     else:
