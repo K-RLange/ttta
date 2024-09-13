76c07a661d913f7103d6063d91095334fdc0b471
Kai-Robin Lange <101263876+K-RLange@users.noreply.github.com>
2024-09-13 10:57:39 +0200

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from src.ttta.methods.sense_representation import SenseRepresentation
-
-if __name__ == '__main__':
-    sense_embedding = SenseRepresentation(
-        target_word='bank'
-    ).infer_representation()
