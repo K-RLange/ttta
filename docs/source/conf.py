7bc29cb64d9aa5e959a6c555c72a1533b4408cd8
K-RLange <101263876+K-RLange@users.noreply.github.com>
2024-12-11 14:14:53 +0100

    Updated rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
 
 project = 'ttta: Tools for Temporal Text Analysis'
 copyright = '2024, Kai-Robin Lange'
