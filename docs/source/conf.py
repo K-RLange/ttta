e6209c7a8b776940d7157900e8c4d5dc1468e031
K-RLange <101263876+K-RLange@users.noreply.github.com>
2024-12-11 14:19:36 +0100

    Updated rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 project = 'ttta: Tools for Temporal Text Analysis'
 copyright = '2024, Kai-Robin Lange'
